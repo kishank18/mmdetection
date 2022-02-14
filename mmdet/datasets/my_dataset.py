bf203c89783d8e019a8110c87c0d59503bbfaae0
kishank18 <kishan3137@gmail.com>
2022-02-14 07:34:45 +0530

    Revert "Create my_dataset.py"
    This reverts commit de0653791510a3ffacd800dd9159bc816e59c7cb.

--- mmdet/datasets/my_dataset.py
+++
@@ -1,9 +0,0 @@
-from .coco import CocoDataset
-from .registry import DATASETS
-from .xml_style import XMLDataset
-### IMPORT YOUR DATASET FORMAT AND PASS IT IN MYDATASET FUNC
-
-@DATASETS.register_module
-class MyDataset(XMLDataset):
-
-    CLASSES = ('RBC') ## ADD ALL YOUR CLASSES
